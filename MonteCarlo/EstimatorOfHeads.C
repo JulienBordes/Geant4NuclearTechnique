93820b336e689136ac3ea1cf590181f0e9f271ff
JulienBordes <julien.bordes@york.ac.uk>
2025-11-13 14:22:43 +0000

    EstimatorOfHeads.C & EstimatorOfPi.C: clang-format + minor edits

--- MonteCarlo/EstimatorOfHeads.C
+++ MonteCarlo/EstimatorOfHeads.C
@@ -1,68 +1,62 @@
-#include "TRandom3.h"
 #include "TGraph.h"
 #include "TMath.h"
+#include "TRandom3.h"
 
-//void EstimatorOfHeads(int samples, int throws, vector<int> headsinThrows)
-void EstimatorOfHeads(int samples, int throws, int k,int* headsinThrow)
-{
+// void EstimatorOfHeads(int samples, int throws, vector<int> headsinThrows)
+void EstimatorOfHeads(int samples, int throws, int k, int *headsinThrow) {
   vector<int> headsinThrows;
-  
-  for (int j=0;j<k;j++)
-  {
+
+  for (int j = 0; j < k; j++) {
     headsinThrows.push_back(headsinThrow[j]);
   }
-  
+
   TRandom3 rndNum;
-  int bins = (samples<300)?samples:300;
-  TH1F *hist = new TH1F("hist",";Probability;counts;",bins, 0, 1);   
-    
-   constexpr double probhead=0.5;
-  
+  int bins = (samples < 300) ? samples : 300;
+  TH1F *hist = new TH1F("hist", ";Probability;counts;", bins, 0, 1);
+
+  constexpr double probhead = 0.5;
+
   // loop over experiments
-  for (int ii = 0; ii < 500; ii++)
-  {
-    int Tally=0;
-      
-      //loop over samples per experiment
-      for (int i = 0; i < samples; i++)
-      {
-        int numheads = 0;
-        
-        // loop over coin throws
-        for (int j = 0; j < throws; j++)
-        {
-          //count number of heads
-          numheads = (rndNum.Rndm() < probhead) ? numheads+1 : numheads; 
-        }
-        
-        //Check if number of heads in throws equals any of required number of heads and increament Tally
-        for(int j = 0; j < headsinThrows.size(); j++)
-        {
-          if (numheads == headsinThrows[j])
-          Tally++;
-        }
+  for (int ii = 0; ii < 500; ii++) {
+    int Tally = 0;
+
+    // loop over samples per experiment
+    for (int i = 0; i < samples; i++) {
+      int numheads = 0;
+
+      // loop over coin throws
+      for (int j = 0; j < throws; j++) {
+        // count number of heads
+        numheads = (rndNum.Rndm() < probhead) ? numheads + 1 : numheads;
       }
-      
-    //calculate probability
-    double prob = Tally * 1.0/samples;
+
+      // Check if number of heads in throws equals any of required number of
+      // heads and increament Tally
+      for (int j = 0; j < headsinThrows.size(); j++) {
+        if (numheads == headsinThrows[j])
+          Tally++;
+      }
+    }
+
+    // calculate probability
+    double prob = Tally * 1.0 / samples;
     hist->Fill(prob);
   }
 
   // Calculation of true probability based on Binomial prob.
   double trueVal = 0;
 
-  for(int j = 0; j < headsinThrows.size(); j++)
-  {
+  for (int j = 0; j < headsinThrows.size(); j++) {
     trueVal += TMath::Binomial(throws, headsinThrows[j]) *
-     TMath::Power(probhead, headsinThrows[j])
-      * TMath::Power(probhead, throws-headsinThrows[j]);
+               TMath::Power(probhead, headsinThrows[j]) *
+               TMath::Power(probhead, throws - headsinThrows[j]);
   }
-    
+
   TF1 *mygaus =
-   new TF1("mygaus","[0]*TMath::Exp(-0.5*(x-[1])/[2]*(x-[1])/[2])", 0, 1.0);
-   mygaus->SetParName(0,"Amplitude");
-   mygaus->SetParName(1,"#mu");
-   mygaus->SetParName(2,"#sigma");
+      new TF1("mygaus", "[0]*TMath::Exp(-0.5*(x-[1])/[2]*(x-[1])/[2])", 0, 1.0);
+  mygaus->SetParName(0, "Amplitude");
+  mygaus->SetParName(1, "#mu");
+  mygaus->SetParName(2, "#sigma");
 
   mygaus->SetParameters(hist->GetMaximum(), hist->GetMean(), hist->GetRMS());
   mygaus->SetNpx(1000);
@@ -73,9 +67,10 @@ void EstimatorOfHeads(int samples, int throws, int k,int* headsinThrow)
   nline->SetLineColor(kRed);
   nline->SetLineStyle(5);
   nline->SetLineWidth(2);
-  gStyle->SetOptFit(0011);  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(0011);
+  gStyle->SetOptStat(0);
 
-  TCanvas *c1a = new TCanvas("c1a", "Pi", 1200, 600);
+  TCanvas *c1a = new TCanvas("c1a", "Coin Flip Probability", 1200, 600);
   hist->Draw();
   hist->Fit("mygaus", "R");
   nline->Draw("same");
--- MonteCarlo/EstimatorOfPi.C
+++ MonteCarlo/EstimatorOfPi.C
@@ -1,9 +1,8 @@
-#include "TRandom3.h"
 #include "TGraph.h"
 #include "TMath.h"
+#include "TRandom3.h"
 
-void EstimatorOfPi(int samples, int update)
-{
+void EstimatorOfPi(int samples, int update) {
   int nEvents = samples;
   int n = 0;
   int in = 0;
@@ -13,60 +12,50 @@ void EstimatorOfPi(int samples, int update)
 
   TGraph *TG_Inside = new TGraph();
   TGraph *TG_Outside = new TGraph();
-    
-  while (n < nEvents)
-  {
-    if (n%10000 == 0)
-    {
-      fprintf (stderr, "%d Thousand\r ", n/1000);
-      fflush (stderr);
-    }
-    
-    x = 1 - 2*rndNum.Rndm();
-    y = 1 - 2*rndNum.Rndm();
-    
-    double radius = TMath::Sqrt(TMath::Power(x, 2)+TMath::Power(y, 2));
 
-    if (radius < 1)
-    {
-      TG_Inside->Set(TG_Inside->GetN()+1);
-      TG_Inside->SetPoint(TG_Inside->GetN()-1, x, y);
+  while (n < nEvents) {
+    x = 1 - 2 * rndNum.Rndm();
+    y = 1 - 2 * rndNum.Rndm();
+
+    double radius = TMath::Sqrt(TMath::Power(x, 2) + TMath::Power(y, 2));
+
+    if (radius < 1) {
+      TG_Inside->Set(TG_Inside->GetN() + 1);
+      TG_Inside->SetPoint(TG_Inside->GetN() - 1, x, y);
       in++;
     }
-    
-    else
-    {
-      TG_Outside->Set(TG_Outside->GetN()+1);
-      TG_Outside->SetPoint(TG_Outside->GetN()-1, x,y);
+
+    else {
+      TG_Outside->Set(TG_Outside->GetN() + 1);
+      TG_Outside->SetPoint(TG_Outside->GetN() - 1, x, y);
     }
-        
+
     n++;
-    
-    if (n%update == 0)
-    {
-      double pivalue = 4.*in/n;
-      TG_Pi->Set(TG_Pi->GetN()+1);
-      TG_Pi->SetPoint(TG_Pi->GetN()-1, n,pivalue);
+
+    if (n % update == 0) {
+      double pivalue = 4. * in / n;
+      TG_Pi->Set(TG_Pi->GetN() + 1);
+      TG_Pi->SetPoint(TG_Pi->GetN() - 1, n, pivalue);
     }
   }
-  
+
   cout << endl;
-    
+
   TG_Outside->SetFillColor(kBlue);
   TG_Outside->SetFillStyle(3001);
   TG_Outside->SetMarkerStyle(8);
   TG_Outside->SetMarkerColor(kBlue);
-    
+
   TG_Inside->SetFillColor(kRed);
   TG_Inside->SetFillStyle(3001);
   TG_Inside->SetMarkerStyle(8);
-  TG_Inside->SetMarkerColor(kRed);  
-    
+  TG_Inside->SetMarkerColor(kRed);
+
   TG_Pi->SetFillColor(kBlue);
   TG_Pi->SetFillStyle(3001);
   TG_Pi->SetMarkerStyle(8);
   TG_Pi->SetMarkerColor(kBlue);
-    
+
   TCanvas *c1a = new TCanvas("c1a", "Pi", 1200, 600);
   c1a->Divide(2, 1);
   c1a->cd(1);
@@ -78,7 +67,7 @@ void EstimatorOfPi(int samples, int update)
   ha->GetXaxis()->CenterTitle();
   ha->GetYaxis()->CenterTitle();
   ha->GetYaxis()->SetTitleOffset(1.5);
-  ha->GetXaxis()->SetNdivisions(105); 
+  ha->GetXaxis()->SetNdivisions(105);
   ha->GetYaxis()->SetNdivisions(105);
   TG_Outside->Draw("P");
   TG_Inside->Draw("P");
@@ -88,7 +77,7 @@ void EstimatorOfPi(int samples, int update)
   nline->SetLineColor(kRed);
   nline->SetLineStyle(5);
   nline->SetLineWidth(2);
-  TH1F *hb = gPad->DrawFrame(0, 3, nEvents+10, 3.5);
+  TH1F *hb = gPad->DrawFrame(0, 3, nEvents + 10, 3.5);
   gPad->SetTickx(1);
   gPad->SetTicky(1);
   hb->SetXTitle("Number of darts");
@@ -96,7 +85,6 @@ void EstimatorOfPi(int samples, int update)
   hb->GetXaxis()->CenterTitle();
   hb->GetYaxis()->CenterTitle();
   hb->GetYaxis()->SetTitleOffset(1.5);
-
 
   TG_Pi->Draw("P");
   gPad->SetTickx(1);
